dd2186baf7055e9540353184f38408fcd13a176c
fanpian <532726345@qq.com>
2019-10-10 17:30:31 +0800

    增加默认导出

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import arrayToTree from './arrayToTree';
-export { arrayToTree };
+import * as base64Library from './base64Library';
+import * as arrayBufferLibrary from './arrayBufferLibrary';
+
+export default { arrayToTree, base64Library, arrayBufferLibrary };
