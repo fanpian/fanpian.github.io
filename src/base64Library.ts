ecc4e1252e13abd68cfcd3050ebec117f14c086d
fanpian <532726345@qq.com>
2019-10-11 17:14:05 +0800

    base64转成Uint8Array时，解决当source不存在时，报错的问题。当source不存在时，返回空数组。

--- src/base64Library.ts
+++ src/base64Library.ts
@@ -2,12 +2,16 @@
  * base64转成Uint8Array
  * @param {string} str base64字符串
  * @param {boolean=} [subMark=true] subMark 是否截取base64标志.默认值:true.
- * @returns { Uint8Array } Uint8Array字节数组
+ * @returns { Uint8Array } Uint8Array字节数组.如果source不存在则返回空数组
  */
 export const toUint8Array = function toUint8Array(
   source: string,
   subMark: boolean = true
 ): Uint8Array {
+  /** source不存在,返回空数组 */
+  if (!source) {
+    return new Uint8Array(0);
+  }
   let copy = source;
   /** base64标志 */
   const base64Mark = ';base64,';
